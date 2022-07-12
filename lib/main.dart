8d048f5df07b17ad107a35034773cbb82a621a01
Broms <Broms359@yandex.ru>
2022-07-12 22:54:06 +0000

    ios build

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
